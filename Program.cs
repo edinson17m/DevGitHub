90baf4c776e3c25abb5f5acca6c3b848d311f47d
edinson17m <MXrza_bh74@student.itstep.org>
2024-04-22 12:09:03 +0300

    Surname
    add  surname

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+
+
+//dev rep
+
+Console.WriteLine("Введитте свое ФИО");
+
+string fullName = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write(fullName.Split()[0]);
+Console.ResetColor();
+
+Console.Write(" ");
